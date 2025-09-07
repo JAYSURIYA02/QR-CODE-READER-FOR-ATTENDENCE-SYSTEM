9c0804fd6ed53fe8690314700f6483742d7f56d4
JAYSURIYA02 <jaysuriya.rv@gmail.com>
2025-09-07 17:09:09 +0530

    The attendence is added to the list

--- real_time_qrcode_reader_with_attendence_marker.py
+++ real_time_qrcode_reader_with_attendence_marker.py
@@ -22,6 +22,9 @@ while True:
             cv2.polylines(frame, np.array([poly], np.int32), True, (255, 0, 0), 2)
             cv2.putText(frame, text.decode("utf-8"), (rect.left, rect.top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
+            with open("attendance.csv", "a") as f:
+                f.write(text.decode("utf-8")[1:-1] + "\n")
+
     cv2.imshow("QR Code Scanner", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
